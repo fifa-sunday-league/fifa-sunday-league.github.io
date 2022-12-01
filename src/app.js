319ecae359de7bb40f3c39736a6647ac2a82fd7f
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2022-12-01 15:55:09 +0200

    Major fix.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 
 let fifaDate = new Date("Dec 03, 2022 13:00").getTime();
+// let fifaDate = new Date("Dec 01, 2022 15:51").getTime();
 
 let x = setInterval(function () {
     let now = new Date().getTime();
@@ -15,4 +16,11 @@ let x = setInterval(function () {
     document.getElementById('hours').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
+    if(distance <0){
+let cd = document.querySelector('.countDown')
+let heading = document.querySelector('.countDown-heading').classList.add("hide");
+cd.innerHTML = `<p class='fifa-time'> Its FIFA TIME!</p>`
+
+console.log(cd)
+    }
 }, 1000);
