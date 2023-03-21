389cdc2b767fb0cba2d4350d1643365c23ad7506
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2023-03-21 16:14:57 +0200

    Update view.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
+import { fifaPlayers } from "./results.js";
+
 
 
 
 // let fifaDate = new Date("Dec 03, 2022 13:00").getTime();
 // let fifaDate = new Date("Feb 04, 2023 13:00").getTime();
+
 let fifaDate = new Date("March 17, 2023 19:00").getTime();
+
+// console.log(fifaPlayers.borislavNikolov.fifaRecord.length,fifaPlayers.borislavNikolov.fifaRecord)
+// console.log(fifaPlayers.mirkoKarahihov.fifaRecord.length)
+// console.log(fifaPlayers.ivanBalabanov.fifaRecord.length)
+// console.log(fifaPlayers.georgiBalabanov.fifaRecord.length)
+
+console.log(fifaPlayers)
+// for (const player in fifaPlayers) {
+//     console.log('Current player is  - >',player)
+//  let currentPlayerWins = fifaPlayers[player].fifaRecord.filter(x => x.includes('W'));
+//  let currentPlayerDraws = fifaPlayers[player].fifaRecord.filter(x => x.includes('D'));
+//  let currentPlayerLose = fifaPlayers[player].fifaRecord.filter(x => x.includes('L'));
+
+
+//  console.log('Wins - ',currentPlayerWins)
+//  console.log('Draws - ',currentPlayerDraws)
+//  console.log('Lose - ',currentPlayerLose)
+//  console.log('Total matches - ',fifaPlayers[player].fifaRecord.length)
+//  console.log('--------------')
+// }
 
 let x = setInterval(function () {
     let now = new Date().getTime();
@@ -22,6 +45,7 @@ let cd = document.querySelector('.countDown')
 let heading = document.querySelector('.countDown-heading').classList.add("hide");
 cd.innerHTML = `<p class='fifa-time'> Its FIFA TIME!</p>`
 
-console.log(cd)
+
     }
 }, 1000);
+
