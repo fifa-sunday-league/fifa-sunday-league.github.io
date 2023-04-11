273a5b0accf7dda5975f60197ee25efd79bfaa3b
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2023-04-11 16:05:01 +0300

    Update for next event.

--- src/results.js
+++ src/results.js
@@ -15,50 +15,68 @@
 //     attendedEvents:["17.03.23"]
 // }
 
-import { calculate,calculateWinDrawLose } from "../utils/calculator.js";
+import {
+    calculate,
+    calculateWinDrawLose
+} from "../utils/calculator.js";
 
 let fifaPlayers = {
     mK: {
-        fifaRecord: ['D', 'L', 'W', 'W', 'D', 'L', 'W', 'L', 'L', 'L', 'L', 'L', 'L','L'],
+        fifaRecord: ['D', 'L', 'W', 'W', 'D', 'L', 'W', 'L', 'L', 'L', 'L', 'L', 'L', 'L'],
         attendedEventsDates: ["17.03.23"],
         wins: 0,
         draws: 0,
         lose: 0
     },
     gB: {
-        fifaRecord: ['D', 'W', 'L', 'L', 'D', 'W', 'L', 'W', 'L', 'L', 'L', 'L', 'W','W',
-        'L','D', 'L', 'L','L', 'L', 'L', 'D','D','W', 'W','L','W'],
+        fifaRecord: ['D', 'W', 'L', 'L', 'D', 'W', 'L', 'W', 'L', 'L', 'L', 'L', 'W', 'W',
+            'L', 'D', 'L', 'L', 'L', 'L', 'L', 'D', 'D', 'W', 'W', 'L', 'W',
+            'L', 'D', 'L', 'D', 'L', 'W', 'L', 'L', 'D', 'W', 'W', 'W',
+            // L D L D L - W L L - D W W W 09.04
+        ],
         attendedEventsDates: ["17.03.23"],
         wins: 0,
         draws: 0,
         lose: 0
     },
     iB: {
-        fifaRecord: ['D', 'W', 'L', 'L', 'D', 'L', 'W', 'L', 'W', 'W', 'W', 'W', 'W','W',
-        'W', 'D', 'W', 'W','L', 'L', 'L', 'D', 'D','L', 'L','W','L'],
-        attendedEventsDates: ["17.03.23",'25.03.23',],
+        fifaRecord: ['D', 'W', 'L', 'L', 'D', 'L', 'W', 'L', 'W', 'W', 'W', 'W', 'W', 'W',
+            'W', 'D', 'W', 'W', 'L', 'L', 'L', 'D', 'D', 'L', 'L', 'W', 'L',
+            'W', 'W', 'D', 'W', 'L', 'L', 'L', 'W', 'L', 'D', 'W', 'W'
+            // W W D W -       L L L W -             L D W W 09.04
+        ],
+        attendedEventsDates: ["17.03.23", '25.03.23', ],
         wins: 0,
         draws: 0,
         lose: 0
     },
     bN: {
-        fifaRecord: ['D', 'L', 'W', 'W', 'D', 'W', 'L', 'W', 'W', 'W', 'W', 'W', 'L','L',
-        'W', 'D', 'W', 'W','W', 'W', 'W', 'D','D','W', 'W','L','W'],
-        attendedEventsDates: ["17.03.23",'25.03.23'],
+        fifaRecord: ['D', 'L', 'W', 'W', 'D', 'W', 'L', 'W', 'W', 'W', 'W', 'W', 'L', 'L',
+            'W', 'D', 'W', 'W', 'W', 'W', 'W', 'D', 'D', 'W', 'W', 'L', 'W',
+            'W', 'W', 'D', 'D', 'W', 'W', 'W', 'W', 'W', 'D', 'L', 'W'
+            // W W D  -     D  W  W      W  -     W W D L     - W 09.04
+        ],
+        attendedEventsDates: ["17.03.23", '25.03.23'],
         wins: 0,
         draws: 0,
         lose: 0
 
     },
     vP: {
-        fifaRecord: ['L','D', 'L', 'L','W', 'W', 'W', 'D','D','L', 'L','W','L'],
+        fifaRecord: ['L', 'D', 'L', 'L', 'W', 'W', 'W', 'D', 'D', 'L', 'L', 'W', 'L',
+            'L', 'D', 'L', 'D', 'L', 'L', 'W', 'W', 'W', 'D', 'L', 'L',
+            // L - D L D - L L W W W D - L L 09.04
+        ],
         attendedEventsDates: ['25.03.23'],
         wins: 0,
         draws: 0,
         lose: 0
     },
     dM: {
-        fifaRecord: [],
+        fifaRecord: [
+            'L', 'L', 'W', 'D', 'W', 'W', 'L', 'L', 'L', 'L', 'L', 'L'
+            // L L - W D W W - L L L - L L L 09.04
+        ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
@@ -76,14 +94,17 @@ let fifaPlayers = {
 let fifaTeams = {
     bNiB: {
         fifaRecord: ['W', 'W', 'W', 'W',
-        'W', 'D', 'W', 'W'],
+            'W', 'D', 'W', 'W',
+            'W', 'W', 'D'
+            // bI W W D 09.04
+        ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
         lose: 0
     },
     bNmK: {
-        fifaRecord: ['D', 'L', 'W', 'W','L','L'],
+        fifaRecord: ['D', 'L', 'W', 'W', 'L', 'L'],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
@@ -91,14 +112,28 @@ let fifaTeams = {
     },
     bNgB: {
         fifaRecord: ['D', 'W', 'L', 'W',
-        'D', 'W', 'W', 'L','W'],
+            'D', 'W', 'W', 'L', 'W',
+'W','W',
+            //bG W W 09.04
+        ],
+        attendedEventsDates: [],
+        wins: 0,
+        draws: 0,
+        lose: 0
+    },
+    //new team
+    bNdM: {
+        fifaRecord: [
+'D','W','W','L',
+            // bD D W W L
+        ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
         lose: 0
     },
 
-    // D W  L W
+
     mKgB: {
         fifaRecord: ['L', 'L', 'L', 'L'],
         attendedEventsDates: [],
@@ -107,7 +142,7 @@ let fifaTeams = {
         lose: 0
     },
     mKiB: {
-     
+
         fifaRecord: ['D', 'L', 'W', 'L'],
         attendedEventsDates: [],
         wins: 0,
@@ -116,8 +151,11 @@ let fifaTeams = {
     },
 
     iBgB: {
-        fifaRecord: ['D', 'W', 'L', 'L','W','W',
-        'L', 'L', 'L', 'D'],
+        fifaRecord: ['D', 'W', 'L', 'L', 'W', 'W',
+            'L', 'L', 'L', 'D',
+'L','D','W','W',
+            //L D W W 09.04
+        ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
@@ -125,26 +163,68 @@ let fifaTeams = {
     },
     // TO DO Add...
     gBvP: {
-        fifaRecord: ['L', 'D', 'L', 'L'],
+        fifaRecord: ['L', 'D', 'L', 'L',
+            'D', 'L', 'D'
+            // vG - D L D 09.04
+        ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
         lose: 0
     },
     bNvP: {
-        fifaRecord: ['W', 'W', 'W', 'D'],
+        fifaRecord: ['W', 'W', 'W', 'D',
+        'W', 'W', 'D'
+    //bV W W D 09.04
+    ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
         lose: 0
     },
     iBvP: {
-        fifaRecord: ['D', 'L', 'L', 'W','L'],
+        fifaRecord: ['D', 'L', 'L', 'W', 'L',
+        'L', 'L', 'W',
+    // iV - L L W 09.04
+    ],
         attendedEventsDates: [],
         wins: 0,
         draws: 0,
         lose: 0
-    }
+    },
+    //new teams
+    iBdM: {
+        fifaRecord: [
+            'W', 'L'
+
+            // dI W L 09.04
+        ],
+        attendedEventsDates: [],
+        wins: 0,
+        draws: 0,
+        lose: 0
+    },
+    vPdM: {
+        fifaRecord: [
+            'L', 'L', 'L'
+            // vD L L L 09.04
+        ],
+        attendedEventsDates: [],
+        wins: 0,
+        draws: 0,
+        lose: 0
+    },
+    gBdM: {
+        fifaRecord: [
+            'L', 'L', 'L'
+            // gD L L L 09.04
+        ],
+        attendedEventsDates: [],
+        wins: 0,
+        draws: 0,
+        lose: 0
+    },
+
 }
 // let borkoId = 'bN'
 // let ivanId = 'iB'
@@ -154,8 +234,8 @@ let fifaTeams = {
 // let venkoId = 'vP'
 // let rumbaId = 'rR'
 // 'gBvP'
-let playersAbbreviation = ['bN','iB','gB','mK','dM','vP','rR'];
-let teamsAbbreviation = ['bNmK','bNgB','bNiB','mKgB','mKiB','iBgB','bNvP','gBvP','iBvP'];
+let playersAbbreviation = ['bN', 'iB', 'gB', 'mK', 'dM', 'vP', 'rR'];
+let teamsAbbreviation = ['bNmK', 'bNgB', 'bNiB', 'mKgB', 'mKiB', 'iBgB', 'bNvP', 'gBvP', 'iBvP','gBdM','bNdM','iBdM','vPdM'];
 // old working calc
 // for (const player in fifaPlayers) {
 //     console.log('Current player is  - >', player)
@@ -203,14 +283,14 @@ calculateWinDrawLose(fifaPlayers)
 calculateWinDrawLose(fifaTeams)
 
 playersAbbreviation.forEach(player => {
-    calculate(fifaPlayers,player)
+    calculate(fifaPlayers, player)
 });
 // calculate(fifaTeams,'bNiB')
 // console.log(fifaTeams)
 // console.log(teamsAbbreviation)
 teamsAbbreviation.forEach(team => {
-    calculate(fifaTeams,team)
-    console.log('team - >',team)
+    calculate(fifaTeams, team)
+    console.log('team - >', team)
 
 });
 
@@ -225,7 +305,9 @@ teamsAbbreviation.forEach(team => {
 // calculate(rumbaId)
 
 
-export { fifaPlayers as fifaPlayers }
+export {
+    fifaPlayers as fifaPlayers
+}
 
 // 'D', 'L', 'W', 'L'
 // 'W', 'W', 'W', 'W'
@@ -241,24 +323,24 @@ export { fifaPlayers as fifaPlayers }
 //     playerName = document.querySelector(`#${id}`)
 
 //     playerName.querySelector('.player-played').textContent = objectForCalc[id].played;
-   
+
 //     if (objectForCalc[id].wins < 10) {
 //         playerName.querySelector('.player-wins').textContent = ' ' + objectForCalc[id].wins;
-    
+
 //     }
 //     else {
 //         playerName.querySelector('.player-wins').textContent = objectForCalc[id].wins;
 //     }
-    
+
 //     if (objectForCalc[id].draws < 10) {
 //         playerName.querySelector('.player-draws').textContent = ' ' + objectForCalc[id].draws;
-    
+
 //     }
 //     else {
 //         playerName.querySelector('.player-draws').textContent = objectForCalc[id].draws;
 //     }
-    
-    
+
+
 //     if(objectForCalc[id].lose <10){
 //         playerName.querySelector('.player-lose').textContent = ' ' + objectForCalc[id].lose;
 //     }
@@ -276,11 +358,11 @@ export { fifaPlayers as fifaPlayers }
 //         objectForCalc[record].wins = currentPlayerWins.length;
 //         let currentPlayerDraws = objectForCalc[record].fifaRecord.filter(x => x.includes('D'));
 //         objectForCalc[record].draws = currentPlayerDraws.length;
-    
+
 //         let currentPlayerLose = objectForCalc[record].fifaRecord.filter(x => x.includes('L'));
 //         objectForCalc[record].lose = currentPlayerLose.length;
 //         objectForCalc[record].attendedEvents = objectForCalc[record].attendedEventsDates.length;
 //         objectForCalc[record].played = objectForCalc[record].fifaRecord.length;
-    
+
 //     }
 // }
